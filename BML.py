9b92e3adfb1fcf4052936060a99b177c71332459
hughec1329 <hughcrockford@gmail.com>
2013-02-09 14:10:56 -0800

    got draw red and blue to work, still got weird bar on lhs of plot?

--- BML.py
+++ BML.py
@@ -4,22 +4,25 @@ data = np.zeros( (1024,1024,3))
 data[3,] = [255,0,0]
 img = smp.toimage( data )
 img.show()
-def draw(n,l):
+def draw(p,n):
+	posx = np.repeat(np.arange(n,dtype=int),n) 	# possible x
+	posy = np.tile(np.arange(n,dtype=int),n)
+	poss = np.vstack((posx,posy))
+	poscar = np.arange(n*n)                 # index of cats
+	np.random.shuffle(poscar)               # randomise (Wasnt working on 2dim)
+	ncars = np.round(n*n*p)
+	carrs = poss[:,poscar[0:ncars]]         # choose random cars
+	cols = np.repeat(np.arange(1,3),ncars/2)
+	cars = np.vstack((carrs,cols))          # colorize
+	
 	dat = np.zeros( (n,n,3))
-	dat[l,] = [255,0,0]
+	redcar = cars[:,cars[2,] == 1]
+	bluecar = cars[:,cars[2,] == 2]
+	dat[redcar[0,:],redcar[1:],] = [255,0,0]
+	dat[bluecar[0,:],bluecar[1:],] = [0,0,255]
+
 	img = smp.toimage( dat )
 	img.show()
 
-posx = np.repeat(np.linspace(1,n,n),n)
-posy = np.tile(np.linspace(1,n,n),n)
-poss = np.vstack((posx,posy))
-poscar = np.arange(n*n)
-np.random.shuffle(poscar)
-ncars = np.round(n*n*p)
-cars = poss[:,poscar[0:ncars]]
-cols = np.repeat(np.arange(1,3),ncars/2)
-carrs = np.vstack((cars,cols))
 
-cars = np.random.randint(0,n*n,size = ncars)
 
-pos = np.matrix(poss).transpose()
